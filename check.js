dcc456d20e1801ef82e1105336c2c68bbd26a6a1
Insanitogames <67851958+Insanitogames@users.noreply.github.com>
2020-09-29 19:07:22 +1300

    Intial commit

---
+++ check.js
@@ -0,0 +1,28 @@
+if(!window.location === "/bots/vxrp.html") {
+    window.location === "/error.html"
+}
+
+if(!window.location === "/bots/new.html") {
+    window.location === "/error.html"
+}
+
+if(!window.location === "/bots/submitbot.html") {
+    window.location.replace("/error.html");
+}
+
+if(!window.location === "/bots/syncio.html") {
+    window.location.replace("/error.html");
+}
+
+if(!window.location === "/index.html") {
+    window.location.replace("/error.html");
+}
+
+if(!window.location === "/welcome.html") {
+    window.location.replace("/error.html");
+}
+
+
+
+
+
