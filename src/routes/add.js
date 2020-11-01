1ee8bf464de4914f584e5a558b94aa45d9cf850e
Infinity <67851958+Infinityisgreat@users.noreply.github.com>
2020-11-01 13:36:00 +1300

    dead

--- src/routes/add.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const route = Router();
-
-route.get("/", async (req, res, next) => {
-    res.render("add", {user: req.user})
-});
-
-module.exports = route;
--- src/routes/success.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const route = Router();
-
-route.get("/", async (req, res, next) => {
-    res.render("success", {user: req.user})
-});
-
-module.exports = route;
